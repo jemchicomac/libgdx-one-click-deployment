0a3b4c28eeaf45a92f85f89cc6dd1501cac696cb
Patricio Gonzalez Sevilla <patriciog@gmail.com>
2014-06-27 19:52:00 +0200

    Sample project added.

---
+++ desktop/src/com/jemchicomac/one-click-deployment/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jemchicomac.one-click-deployment.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jemchicomac.one-click-deployment.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
